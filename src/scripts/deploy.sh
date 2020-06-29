3684dc7f5c62f5d76ee9e862ca39e124a44b3c21
Thomas Combarnous <thomas.combarnous@gekko.fr>
2020-06-29 22:39:55 +0200

    test lambdas

--- src/scripts/deploy.sh
+++ src/scripts/deploy.sh
@@ -7,7 +7,10 @@ WORKDIR=$(pwd)
 
 echo "Deploying Lambdas 1 & 2"
 
-cd $WORKDIR/src/demo-lambda/python/
+cd $WORKDIR/src/demo-lambda/python/lambda-test-1
+sls deploy --variant lab --region eu-west-3 --force --log-level debug
+
+cd $WORKDIR/src/demo-lambda/python/lambda-test-2
 sls deploy --variant lab --region eu-west-3 --force --log-level debug
 
 echo "Done with deploying lambdas 1 & 2"
