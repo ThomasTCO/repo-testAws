d630e7150729c2dca4accfeeee64d7fea9ec990a
Thomas Combarnous <thomas.combarnous@gekko.fr>
2020-06-29 16:19:42 +0200

    test pipeline

--- src/scripts/deploy.sh
+++ src/scripts/deploy.sh
@@ -7,7 +7,9 @@ NUM_COMPTE='284952015401'
 
 
 echo "Deploying Hello World..."
-cd src/demo-lambda/python/
+echo "FromDir: $WORKDIR"
+
+cd ../demo-lambda/python/
 sls deploy --variant lab --region eu-west-3 --force --log-level debug
 sls_status=$?
 echo "Done with deploying Hello World..."
