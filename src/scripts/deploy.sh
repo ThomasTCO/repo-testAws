8d6f8ed44eb62569a25c71f6e227becc07553b71
Thomas Combarnous <thomas.combarnous@gekko.fr>
2020-06-30 14:27:07 +0200

    deploy severals lambdas

--- src/scripts/deploy.sh
+++ src/scripts/deploy.sh
@@ -7,12 +7,19 @@ WORKDIR=$(pwd)
 
 echo "Deploying Lambdas 1 & 2"
 
-echo "Lambda 1 deploy"
-cd $WORKDIR/src/demo-lambda/python/lambda-test1
-sls deploy --variant lab --region eu-west-3 --force --log-level debug
+cd $WORKDIR/src/demo-lambda/python/
+for lambda in $(ls .); do
+    echo "Deploying $lambda"
+    sls deploy --variant lab --statictarget $STATIC_TAR --datatype $lambda
+done
 
-echo "Lambda 2 deploy"
-cd $WORKDIR/src/demo-lambda/python/lambda-test2
-sls deploy --variant lab --region eu-west-3 --force --log-level debug
+
+#echo "Lambda 1 deploy"
+#cd $WORKDIR/src/demo-lambda/python/lambda-test1
+#sls deploy --variant lab --region eu-west-3 --force --log-level debug
+
+#echo "Lambda 2 deploy"
+#cd $WORKDIR/src/demo-lambda/python/lambda-test2
+#sls deploy --variant lab --region eu-west-3 --force --log-level debug
 
 echo "Done with deploying lambdas 1 & 2"
