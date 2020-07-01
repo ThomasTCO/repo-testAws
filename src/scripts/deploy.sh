a06bd956fb647a2aeeb7c72ca84d91e6f68ec5ce
Thomas Combarnous <thomas.combarnous@gekko.fr>
2020-07-01 11:24:14 +0200

    JKfile

--- src/scripts/deploy.sh
+++ src/scripts/deploy.sh
@@ -9,17 +9,6 @@ LAMBDA_DEPLOY="lambda-test"
 echo "Deploying Lambdas 1 & 2"
 
 cd $WORKDIR/src/demo-lambda/python/
-#for lambda in $(ls .); do
 sls deploy --userenv dev --name $LAMBDA_DEPLOY --region eu-west-3 --force --log-level debug
-#done
-
-
-#echo "Lambda 1 deploy"
-#cd $WORKDIR/src/demo-lambda/python/lambda-test1
-#sls deploy --variant lab --region eu-west-3 --force --log-level debug
-
-#echo "Lambda 2 deploy"
-#cd $WORKDIR/src/demo-lambda/python/lambda-test2
-#sls deploy --variant lab --region eu-west-3 --force --log-level debug
 
 echo "Done with deploying lambdas 1 & 2"
